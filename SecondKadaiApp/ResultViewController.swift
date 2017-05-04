b99480b4012ca6635708700105c8d7eea3234053
Skybridge7 <hiroko@Hirokos-MacBook-Pro.local>
2017-05-04 17:53:38 +0900

    エラー内容確認のためお送りします。

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Hiroko Narita on 2017/05/03.
+//  Copyright © 2017 Skybridge7. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    @IBAction func handle(_ sender: AnyObject) {
+        label.text = "こんにちは、 name　さん"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    class MasterViewController: UIViewController {
+        //メソッド：遷移先から戻る
+        func unwind(){
+        }
+        @IBAction func unwind(segue: UIStoryboardSegue) { }
+    }
 }
-
